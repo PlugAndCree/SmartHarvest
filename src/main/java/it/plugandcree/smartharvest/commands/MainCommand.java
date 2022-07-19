4327e4277ba17d9ededfc30e9e1c4501e7aba7d0
PlugAndCree <nicolamarangon123@gmail.com>
2022-07-19 17:35:34 +0200

    Fixed cmd inheritance bug

--- src/main/java/it/plugandcree/smartharvest/commands/MainCommand.java
+++ src/main/java/it/plugandcree/smartharvest/commands/MainCommand.java
@@ -17,10 +17,14 @@ public class MainCommand extends Command {
 
 	@Override
 	public boolean execute(CommandSender sender, ExecutionContext context) {
+		if(!context.isLastCommand())
+			return false;
+		
 		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a&lSMART HARVEST &7plugin made by &aPlug_And_Cree"));
 		if(sender.hasPermission("smartharvest.help"))
 			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7Type &a/smartharvest help &7for more info"));
-		return context.isLastCommand();
+		
+		return true;
 	}
 	
 }
